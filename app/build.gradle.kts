64324a5d6902bb1d57b791d5ef24d31a9f8bee6a
josh-clsn <josh-clsn@users.noreply.github.com>
2026-05-05 19:35:40 -0400

    release: bump to v0.2.3 (versionCode 9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.etchit"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "0.2.2"
+        versionCode = 9
+        versionName = "0.2.3"
 
         buildConfigField("String", "REOWN_PROJECT_ID", "\"$reownProjectId\"")
         buildConfigField("long", "CHAIN_ID", "42161L")
